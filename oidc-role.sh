27af6635a78dda4e98c78172256ccec46f2c09d3
ketanj <joshi.ketan@tabcorp.com.au>
2026-03-15 13:59:34 +1100

    Added CDK Code

--- oidc-role.sh
+++ oidc-role.sh
@@ -4,5 +4,11 @@ aws cloudformation deploy \
   --template-file github-oidc-role.yml \
   --stack-name github-actions-oidc \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides GitHubOrg=Ketan-Joshi GitHubRepo=github-action-test \
-  --region us-east-1
+  --region us-east-1 \
+  --profile ketan \
+  --parameter-overrides \
+    GitHubOrg=Ketan-Joshi \
+    GitHubRepo=github-action-test \
+    AccountId=$(aws sts get-caller-identity --profile ketan --query Account --output text) \
+    CreateOidcProvider=true \
+    BranchName=main
